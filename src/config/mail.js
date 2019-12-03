70b0ac6242c2fafc007e3dacb9d457f2d8eac54f
wagner.a.filgueiras@gmail.com <wagner.a.filgueiras@gmail.com>
2019-12-02 21:06:21 -0700

    Appointment cancelation and email notification

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4054aad782cc35',
+    pass: '9325f503e596ad',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
